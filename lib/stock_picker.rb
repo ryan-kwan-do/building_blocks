d2de867ef92a270fab5cb3f49e0530d249d73bc4
Ryan Armstrong <hello@ryanarmstrong.me>
2016-05-09 13:31:56 +0200

    finished picker, added test

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -1,8 +1,24 @@
-def stock_picker(prices)
-	
-low_price = prices.find_index(prices.min)
-high_price = prices.find_index(prices.max)
+def stock_picker(prices)	
+#creates a new array to manipulate
+new = prices
 
-selection = [low_price, high_price]
+#accounts for min values at the end of the array by removing these
+while new.size > 0 && new.min == new.last
+ new.pop
+end
+
+#if prices array has decreasing values, recommendation is nil
+if new.size == 0
+	selection = [nil,nil]
+#otherwise it picks the index of the min and max	
+else	
+	min_price = new.min
+	min_price_index = new.find_index(new.min)
+	max_options = new.drop(min_price_index + 1)
+	max_price_index = max_options.find_index(max_options.max) + min_price_index + 1
+	selection = [min_price_index, max_price_index]
+end
+
 selection
+
 end
--- spec/stock_picker_spec.rb
+++ spec/stock_picker_spec.rb
@@ -6,8 +6,9 @@ Implement a method #stock_picker that takes in an array of stock prices,
 one for each hypothetical day. It should return a pair of days representing 
 the best day to buy and the best day to sell. 
 Days start at 0.
-My own addition:
- - The program always buys and sells, even when the minimum is on the last day
+My notes and interpretations:
+- The best days are those that result in the highest profit
+- Given falling prices, program should recommend not buying
 =end
 
 describe "stock_picker"   do
@@ -18,15 +19,20 @@ describe "stock_picker"   do
 	end
 
 	context "Minimum comes after the maximum price" do
-		it "Returns the index of the min day, 
-			and the index of the next max day" do
+		it "Returns the next pair that returns a the highest profit" do
 			expect(stock_picker([20,1,5,2,3,4,7])).to eql([1,6])
 		end
 	end
 
 	context "The minimum price falls on the last day" do
-		it "Chooses the min before the last day, and the max after that min" do
+		it "Returns the minimum before that day and the max after that min" do
 			expect(stock_picker([20,5,2,3,4,7,1])).to eql([2,5])
+		end
+	end
+
+	context "Prices fall every day" do
+		it "Returns nil values for each day" do
+			expect(stock_picker([6,5,4,3,2,1,0])).to eql([nil,nil])
 		end
 	end
 
